0c8dda37ab2be074874fe9c19f091b7c7b528e1e
DeadlyOP17 <109978087+DeadlyOP17@users.noreply.github.com>
2024-07-20 12:13:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7238178518:AAG-Agu6vGjxc0xSpasHmEe5Pe8b519FUOs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("StanWTF")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7223419712")
 
 
 
